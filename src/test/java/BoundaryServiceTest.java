b4007fcf15576010ff45a86b78edc83d5157aa10
”Leonid <surpraiz50@gmail.com>
2023-08-14 20:17:40 +0300

    Полный билд

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -1,22 +1,16 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.example.BoundaryService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BoundaryServiceTest {
-
     @Test
-    public void testFindMinWithSingleNumber() {
-        int[] marks = {10};
-        BoundaryService boundaryService = new BoundaryService();
-        int result = boundaryService.findMin(marks);
-        assertEquals(10, result);
-    }
-    @Test
+    @DisplayName("Минимальное значение для положительных чисел")
     public void testFindMinWithPositiveNumbers() {
         int[] marks = {10, 5, 8, 3, 7};
         BoundaryService boundaryService = new BoundaryService();
         int result = boundaryService.findMin(marks);
-        assertEquals(3, result);
+        int expected = 3;
+        assertEquals(expected, result);
     }
 }
